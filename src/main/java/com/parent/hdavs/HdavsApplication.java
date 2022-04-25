b6b24da65229183b3b3e3653002cebd896839a0e
crj-tik <632238575@qq.com>
2022-04-25 08:03:17 +0800

    0-0 项目搭建

---
+++ src/main/java/com/parent/hdavs/HdavsApplication.java
@@ -0,0 +1,13 @@
+package com.parent.hdavs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdavsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdavsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parent/hdavs/HdavsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parent.hdavs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdavsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
